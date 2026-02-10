f5bb27dd80149a683ca4382c9a45e95d5b62d620
Sagar Arora <arorasagar540@gmail.com>
2026-02-11 04:55:39 +0530

    Increase server payload limit to 500MB

--- server/server.js
+++ server/server.js
@@ -23,8 +23,8 @@ const io = new Server(server, {
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 // Routes Imports
 import examRoutes from './routes/exam.js';
